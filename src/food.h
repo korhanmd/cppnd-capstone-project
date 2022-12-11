5ccc569b6623b68a12fbb613b63f8a7f6bbad31f
Korhan Mutludoğan <korhanmd@gmail.com>
2022-12-11 15:56:04 +0300

    Add vector of threads to the food class

--- src/food.h
+++ src/food.h
@@ -2,10 +2,15 @@
 #define FOOD_H
 
 #include "SDL.h"
+#include <vector>
+#include <thread>
 
 class Food : public SDL_Point {
  public:
   Food();
+
+ protected:
+  std::vector<std::thread> threads;
 
  private:
 };
